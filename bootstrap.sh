3f4bf74bbab760e8f1f81ad5b38f732b6c809c1b
rajatrockers007 <134793294+rajatrockers007@users.noreply.github.com>
2023-12-08 13:30:55 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-
+addgroup rajat
+useradd rajat --create-home --shell /bin/bash --groups rajat
+#gpasswd -a rajat sudo #allowing sudo requires password, and not a good idea for a service account.
+mkdir /home/rajat/.ssh
+cat <<EOL >> /home/rajat/.ssh/authorized_keys
+# Add your public SSH keys here, one per line
+ssh-rsa YOUR_PUBLIC_KEY_1 comment1
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCD0ILBlvXb9hE76hOXwls9j32UcxlGs9uvCw9ozBalROkvwoYcCpfS4WAb1E+Izu2K70x935coOlRw5iMEdHrojP3tjb6FD4oU/nGkEXyNVjplGuHCSiYMF6xEDANjOvm/affzHv12fMhqVqyQ6fvtNGYCXB2VqBkbHtMr5iQK65lffb6RelCpvcsEZ1S6JNBjwkH+L81ZWTJEMMP/oJelSd+yVLok9HooAXHlQAYA7YFHzfQGHboMl2iuMlQXzwCr9abWDy5WIU8/8R7uXYBBwHP57rE1K0DRrpXePSMufKxN1IxZXA8twFisbt+287lsHllK5DkSS7ypEwvJ6TNuOKgej1zzjUPh8ep5TI+5XBsPQRPoM6HJxpls4fsyzaiPy5neF2UmC9rWSqlkvHLEK1BdrlXohNjnnjPbRBYvb6RTpGrjECxMnXCyEVTVtw4HoHz8s3jNicn/jcNOBH3FYXK7PJxwrYqTwPjYWQ/TPzvDRmCI0AuUUsc742YTAPE= rajat@LXC-Docker
+ssh-rsa YOUR_PUBLIC_KEY_2 comment2
+# Add more keys as needed
+EOL
+chmod 700 /home/rajat/.ssh
+chown rajat:rajat /home/rajat -R
 # Determine the package manager and service management commands
 if [[ -x "$(command -v apt)" ]]; then
     # Debian/Ubuntu-based systems
